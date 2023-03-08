d74a57ac3d71e009d8a17129369c60363d108956
Rob Jenkins <rwjenkins@gmail.com>
2023-03-08 11:19:58 -0500

    refactor

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [ myInput, setMyInput ] = useState('');
+
+  const myData = {
+    messages: [
+      { text: "Hey, sup?" },
+      { text: "Not much. Salty over the sb" }
+    ]
+  };
+
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>New Message:</Text>
+      <TextInput />
+      <Button />
+
+      <Text>My messages:</Text>
+      <FlatList />
       <StatusBar style="auto" />
     </View>
   );
